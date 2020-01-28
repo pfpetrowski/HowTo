feb2d16ed6d50e7d00d962aff2eb21c966331a6d
Paul Petrowski <pfpetrowski@gmail.com>
2020-01-28 10:22:28 -0600

    updated create_markerIDs

--- create_markerIDs.sh
+++ create_markerIDs.sh
@@ -1,1 +1,4 @@
 bcftools annotate --set-id +'%CHROM\_%POS\_%REF\_%FIRST_ALT' glyma.Wm82.gnm1.div.QXZK.SNPData.vcf.gz | bgzip > glyma.Wm82.gnm1.div.QXZK.SNPData.markerIDsAdded.vcf.gz
+
+#Use this to overwrite any marker IDs that were there originally
+bcftools annotate -x ID glyma.Wm82.gnm1.div.QXZK.SNPData.vcf.gz | bcftools annotate --set-id +'%CHROM\_%POS\_%REF\_%FIRST_ALT' | bgzip > glyma.Wm82.gnm1.div.QXZK.SNPData.markerIDsAdded.ogRemoved.vcf.gz
