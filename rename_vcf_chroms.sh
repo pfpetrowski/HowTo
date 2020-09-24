b459a3fb2f9f63569682a6e317a8db8691a25ed2
Paul Petrowski <ppetrowski@bensonhill.com>
2020-09-24 17:15:20 -0500

    included new method for renaming chromosomes using bcftools

--- rename_vcf_chroms.sh
+++ rename_vcf_chroms.sh
@@ -2,3 +2,12 @@ zcat myvcf.vcf.gz | sed 's/^Chr/Gm/' | sed 's/<ID=Chr/<ID=Gm/' | bgzip -c > myvc
 
 #'Chr' is the string we want to replace
 #Gm is the replacement
+
+
+#Another way
+bcftools annotate -Oz --rename-chrs fixChrom.txt --threads 16  imputed.fullsequences_tagsremoved_sorted_markerIDs.vcf.gz -o imputed.fullsequences_tagsremoved_sorted_markerIDs_fixChrom.vcf.gz
+
+#fixChrom.txt is a text file containing the new mapping in the form of
+#oldval newval \n pairs
+# --threads is optional
+# -Oz specifies that the output file is to be gzipped. use -Ov for a normal vcf
